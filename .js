e2e2fd7489edf3749a42caf3002091f52396284b
ram-1258 <sujitkushwaha6818@gmail.com>
2025-08-03 11:36:28 +0530

    Update .js

--- .js
+++ .js
@@ -1,2 +1,27 @@
+let output = document.getElementById("res");
+
+// Add characters to the input field
+function Solve(val) {
+    output.value += val;
+}
+
+// Clear the input field
+function Clear() {
+    output.value = "";
+}
+
+// Remove the last character
+function Back() {
+    output.value = output.value.slice(0, -1);
+}
+
+// Evaluate the expression
+function Result() {
+    try {
+        output.value = eval(output.value);
+    } catch (e) {
+        output.value = "Error";
+    }
+}
 
 
